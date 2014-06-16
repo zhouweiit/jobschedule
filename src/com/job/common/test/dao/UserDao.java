05359e99ab2ae07aafad5bd7b94f6d3cebb69ae0
zhouweiit <zhouwei.it@gmail.com>
2014-06-16 21:51:36 +0800

    UserDao

--- src/com/job/common/test/dao/UserDao.java
+++ src/com/job/common/test/dao/UserDao.java
@@ -4,17 +4,48 @@ import java.util.List;
 
 import job.framework.lang.JobException;
 
-
 import com.job.common.test.entity.User;
 
+
+/**
+ * 用户信息数据操作DAO
+ * @author zhouwei
+ *
+ */
 public interface UserDao {
 	
+	/**
+	 * 根据用户ID获取用户信息
+	 * @param id 用户ID
+	 * @return
+	 * @throws JobException
+	 * @author zhouwei
+	 */
 	public User getUserById(int id) throws JobException;
 
+	/**
+	 * 获取所有用户信息
+	 * @param dos
+	 * @return
+	 * @throws JobException
+	 * @author zhouwei
+	 */
 	public List<User> getAllUser(int dos) throws JobException;
 
+	/**
+	 * 更新一条用户信息
+	 * @param user
+	 * @throws JobException
+	 * @author zhouwei
+	 */
 	public void updateUser(User user) throws JobException;
 	
+	/**
+	 * 插入一条用户信息
+	 * @param user
+	 * @throws JobException
+	 * @author zhouwei
+	 */
 	public void insertUser(User user) throws JobException;
 
 }
